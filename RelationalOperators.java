10879cf94a9d24ad7bb32207bce9f5ed4e329e12
knshifa <eng25am1009@dsu.edu.in>
2025-11-30 15:52:24 +0530

    Create RelationalOperators.java

---
+++ RelationalOperators.java
@@ -0,0 +1,12 @@
+public class RelationalOperators {
+    public static void main(String[] args) {
+        int x = 50, y = 60;
+
+        System.out.println("x == y: " + (x == y));
+        System.out.println("x != y: " + (x != y));
+        System.out.println("x > y : " + (x > y));
+        System.out.println("x < y : " + (x < y));
+        System.out.println("x >= y: " + (x >= y));
+        System.out.println("x <= y: " + (x <= y));
+    }
+}
